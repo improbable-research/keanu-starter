1c39faa2c9eeabebc9df16a90e0ec1d0bf092329
CharlieCrisp <charliecrisp@improbable.io>
2019-01-17 13:30:00 +0000

    Kotlin

---
+++ src/main/kotlin/com/example/starter/KotlinMain.kt
@@ -0,0 +1,7 @@
+package com.example.starter
+
+fun main(args : Array<String>) {
+    println("Hello Kotlin")
+}
+
+class KotlinMain {}
---
+++ src/test/kotlin/com/example/starter/TestKotlinMain.kt
@@ -0,0 +1,11 @@
+package com.example.starter
+
+import org.junit.Assert
+import org.junit.Test
+
+class TestKotlinMain {
+    @Test
+    fun testMain() {
+        Assert.assertTrue(true)
+    }
+}
